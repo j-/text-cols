356f5dd9070efadcb75650bab65a404c44fbff2d
j- <j@skeoh.com>
2017-02-11 21:20:39 +1100

    Add App component

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const App = () => (
+	<div className="app">
+		<section id="section-config">
+			<form id="config">
+				<div className="form-group">
+					<label htmlFor="config-cols">Cols</label>
+					<input id="config-cols" type="number" min="1" />
+				</div>
+				<div className="form-group">
+					<label htmlFor="config-tabwidth">Tab width</label>
+					<input id="config-tabwidth" type="number" min="1" />
+				</div>
+			</form>
+		</section>
+		<div id="ruler-horizontal" className="ruler"></div>
+		<section id="section-splitter">
+			<textarea id="text-input" className="pane pane-left" placeholder="Type here..."></textarea>
+			<div className="drag-handle"></div>
+			<section className="pane pane-right"></section>
+		</section>
+	</div>
+);
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
 import './styles';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
 
 var currentCols = 80;
 
