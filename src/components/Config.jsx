aa29faf96e5ceab965cf8ddd8b57af2a639b0092
j- <j@skeoh.com>
2017-02-12 00:23:29 +1100

    Remove ID from config form

--- src/components/Config.jsx
+++ src/components/Config.jsx
@@ -8,40 +8,38 @@ const Config = ({
 	setTabSize,
 	setFontSize,
 }) => (
-	<section id="section-config">
-		<form id="config">
-			<div className="form-group">
-				<label htmlFor="config-cols">Cols</label>
-				<input
-					id="config-cols"
-					type="number"
-					min="1"
-					value={ cols }
-					onChange={ (e) => setCols(e.target.value) }
-				/>
-			</div>
-			<div className="form-group">
-				<label htmlFor="config-tabwidth">Tab width</label>
-				<input
-					id="config-tabwidth"
-					type="number"
-					min="1"
-					value={ tabSize }
-					onChange={ (e) => setTabSize(e.target.value) }
-				/>
-			</div>
-			<div className="form-group">
-				<label htmlFor="config-fontsize">Font size (px)</label>
-				<input
-					id="config-fontsize"
-					type="number"
-					min="1"
-					value={ fontSize }
-					onChange={ (e) => setFontSize(e.target.value) }
-				/>
-			</div>
-		</form>
-	</section>
+	<form className="config">
+		<div className="form-group">
+			<label htmlFor="config-cols">Cols</label>
+			<input
+				id="config-cols"
+				type="number"
+				min="1"
+				value={ cols }
+				onChange={ (e) => setCols(e.target.value) }
+			/>
+		</div>
+		<div className="form-group">
+			<label htmlFor="config-tabwidth">Tab width</label>
+			<input
+				id="config-tabwidth"
+				type="number"
+				min="1"
+				value={ tabSize }
+				onChange={ (e) => setTabSize(e.target.value) }
+			/>
+		</div>
+		<div className="form-group">
+			<label htmlFor="config-fontsize">Font size (px)</label>
+			<input
+				id="config-fontsize"
+				type="number"
+				min="1"
+				value={ fontSize }
+				onChange={ (e) => setFontSize(e.target.value) }
+			/>
+		</div>
+	</form>
 );
 
 Config.propTypes = {
